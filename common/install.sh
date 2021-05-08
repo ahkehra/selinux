42c78279c2c1fb2ac760543d8cb53ebfa71f74ca
akira-vishal <vishal.rockstar7011@gmail.com>
2021-05-08 11:44:54 +0630

    common: change enforce to enforcing

--- common/install.sh
+++ common/install.sh
@@ -20,18 +20,18 @@ ui_print ""
 sleep 0.5
 ui_print "- Security-Enhanced Linux (SELinux) -"
 if [ "$(su -c getenforce)" == "Permissive" ]; then
-    sed -i '/setenforce/s/.*/setenforce 0/' $MODPATH/post-fs-data.sh
-    sed -i "s/echo/echo 0/g" $MODPATH/post-fs-data.sh
-    sed -i "s/Magisk Module providing basic SELinux Managment functionality./Magisk Module providing basic SELinux Managment functionality. Security-Enhanced Linux Mode: Permissive/g" $MODPATH/module.prop
-    echo ""
-    echo "- SELinux Permissive Mode"
-    echo ""
-elif [ "$(su -c getenforce)" == "Enforce" ]; then
     sed -i '/setenforce/s/.*/setenforce 1/' $MODPATH/post-fs-data.sh
     sed -i "s/echo/echo 1/g" $MODPATH/post-fs-data.sh
     sed -i "s/Magisk Module providing basic SELinux Managment functionality./Magisk Module providing basic SELinux Managment functionality. Security-Enhanced Linux Mode: Enforcing/g" $MODPATH/module.prop
     echo ""
     echo "- SELinux Enforcing Mode"
+    echo ""
+elif [ "$(su -c getenforce)" == "Enforcing" ]; then
+    sed -i '/setenforce/s/.*/setenforce 0/' $MODPATH/post-fs-data.sh
+    sed -i "s/echo/echo 0/g" $MODPATH/post-fs-data.sh
+    sed -i "s/Magisk Module providing basic SELinux Managment functionality./Magisk Module providing basic SELinux Managment functionality. Security-Enhanced Linux Mode: Permissive/g" $MODPATH/module.prop
+    echo ""
+    echo "- SELinux Permissive Mode"
     echo ""
 fi
 mv -f $MODPATH/common/akirasuper $MODPATH/system/bin/akirasuper
